2b1331c7c1a825d2dca9c109f25e156f2cdd839f
UrveshVernekar <urvesh_vernekar@ifbglobal.com>
2026-04-27 09:02:10 +0530

    Add example .env tamplate

--- app/config.py
+++ app/config.py
@@ -1,14 +1,13 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://user:password@localhost:5432/postgres"
+    DATABASE_URL: str
     JWT_SECRET: str
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
 settings = Settings()
